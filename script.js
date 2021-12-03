a31091dd938fa84c4556f59f75c91c32e7603d66
Gregmo07 <laurentg07@gmail.com>
2021-12-03 22:44:50 +0100

    it's working

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
-let value = '';
+let newValue = true;
 let firstValue = '';
-let operatorChoosed = false;
 let choosedOperator = '';
-let t = 0;
 const display = document.querySelector('.display');
 
 document.querySelector('#clear').addEventListener('click', () => clearAll())
 
+function clearAll() {
+    value = '';
+    firstValue = '';
+    choosedOperator = '';
+    display.innerText = '';
+}
+
+
 document.querySelectorAll(".chiffre").forEach(chiffre =>
     chiffre.addEventListener('click', () => { writeDisplay(chiffre.innerText) }));
 
-
-    function writeDisplay(number) {
-        if (value == '') {
-            display.innerText = '';
-        }
-        value += number;
-        display.innerText += number;
-    
+function writeDisplay(number) {
+    if (newValue) {
+        display.innerText = '';
     }
+    display.innerText += number;
+    newValue = false;
+}
 
 
-    document.querySelectorAll('.operator').forEach(operatorSelected =>
-        operatorSelected.addEventListener('click', () => { chooseOperator(operatorSelected) }));
-    
+document.querySelectorAll('.operator').forEach(operatorSelected =>
+    operatorSelected.addEventListener('click', () => { chooseOperator(operatorSelected) }));
 
 function chooseOperator(operator) {
-    if (choosedOperator == ''){
-        choosedOperator = operator.id
-    }
-    if (operator.id == 'equal' && value != '') {
-        operate(choosedOperator, firstValue, value);
-        firstValue = '';
-    } else if (choosedOperator != 'equal' && operatorChoosed && value != '' && firstValue != '') {
-        
-       operate(choosedOperator, firstValue, value);
-        
-    } else {
-        operatorChoosed = true;
-        
-        if (firstValue ==''){
-            firstValue = value;
+
+    if (display.innerText != '') {
+        if (firstValue == '') {
+            firstValue = display.innerText;
+            choosedOperator = operator.id;
+            newValue = true;
+            return;
         }
-        value = '';
+        if (operator.id == 'equal') {
+            operate(choosedOperator, firstValue, display.innerText);
+            firstValue = '';
+            return;
+        }
+        operate(choosedOperator, firstValue, display.innerText);
+        choosedOperator = operator.id;
     }
 }
 
@@ -71,9 +72,7 @@ function operate(oper, a, b) {
     }
     display.innerText = result;
     firstValue = result;
-    choosedOperator = '';
-    value = '';
-    operatorChoosed = false;
+    newValue = true;
 
 }
 
@@ -98,10 +97,3 @@ function modulo(a, b) {
     return a % b;
 }
 
-function clearAll() {
-    value = '';
-    firstValue = '';
-    choosedOperator = '';
-    operatorChoosed = false;
-    display.innerText = '';
-}
